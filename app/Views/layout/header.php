0ac1efbeafdec1f868c5e75444b88ec11b886c11
ajmspace <asepjayamutakin12@gmail.com>
2023-06-24 02:17:02 +0700

    edit header

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -3,6 +3,7 @@
         <div class="row">
             <div class="col-md-12">
                 <h1 class="h1">Portal Berita Codeigniter</h1>
+                <h4>Asep Jaya Mutakin</h>
             </div>
         </div>
     </div>
--- app/Views/layout/post_layout.php
+++ app/Views/layout/post_layout.php
@@ -5,6 +5,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Protal Berita Codeigniter</title>
+    
 
 	<!-- Bootstrap CSS -->
 	<link rel="stylesheet" href="<?= base_url('css/bootstrap.min.css') ?>" />
